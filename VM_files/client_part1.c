c5801f2aefbb804fa884b7cd58149d6b11500d64
aditya17varma <69626061+aditya17varma@users.noreply.github.com>
2023-03-27 14:45:04 -0700

    backup

--- VM_files/client_part1.c
+++ VM_files/client_part1.c
@@ -7,11 +7,14 @@
 #include <strings.h>
 #include "cJSON.h"
 #include <errno.h>
+#include <stdbool.h>
 
 #define MAX 1500
 #define SA struct sockaddr
 
 int global_payload_size = 1500;
+
+int send_udp_trains(int, struct sockaddr_in, int, int, bool);
 
 struct config_struct {
     in_addr_t Server_IP;
@@ -26,16 +29,12 @@ struct config_struct {
     int UDP_TTL;
 };
 
-// struct udp_payload{
-//     short packet_id;
-//     char payload[global_payload_size - 2];
-// };
-
-struct config_struct* parse_JSON() {
+struct config_struct* parse_JSON(char* filename) {
 
     struct config_struct *config = malloc(sizeof(struct config_struct));
 
-    FILE *fp = fopen("../config.json", "r");
+    FILE *fp = fopen(filename, "r");
+
 
     char buffer[1024];
     fread(buffer, 1, 1024, fp);
@@ -56,9 +55,6 @@ struct config_struct* parse_JSON() {
     if (cJSON_IsString(Server_IP)) {
         config->Server_IP = inet_addr(Server_IP->valuestring);
         // printf("Server IP struct: %d\n", config->Server_IP);
-    }
-    else {
-        printf("Shouldn't get here\n");
     }
 
     cJSON *Source_UDP_Port = cJSON_GetObjectItemCaseSensitive(root, "Source_UDP_Port");
@@ -120,27 +116,55 @@ struct config_struct* parse_JSON() {
 }
 
 void pre_probing (int sockfd, FILE *fp) {
-    char data[MAX];
-    int n;
+    char *read_buff;
 
-    while (fgets(data, MAX, fp) != NULL) {
-        if (send(sockfd, data, sizeof(data), 0) == -1){
-            perror("Client TCP pre-probing error, sending file..");
-            exit(1);
-        }
-        bzero(data, MAX);
+    long lSize;
+    fseek(fp, 0L, SEEK_END);
+    lSize = ftell(fp);
+    rewind(fp);
+
+    read_buff = calloc(1, lSize + 1);
+    if (!read_buff){
+        fclose(fp);
+        perror("read_buff failed...");
+        exit(1);
     }
+    if(1 != fread(read_buff, lSize, 1, fp)){
+        fclose(fp);
+        perror("fread failed...");
+        free(read_buff);
+        exit(1);
+    }
+
+    // printf("read_buff: %s\n", read_buff);
+    // printf("Read_buff size: %ld\n", sizeof(read_buff));
+
+    if (send(sockfd, read_buff, lSize + 1, 0) == -1){
+        perror("Client TCP pre-probing error, sending file..");
+        // printf("%s\n", read_buff);
+        exit(1);
+    }
+
+    free(read_buff);
 }
 
-int main() {
-    // char *ip = "192.168.64.2";
-    struct config_struct *config = parse_JSON();
+int main(int argc, char** argv) {
+    if (argc < 2){
+        perror("Please include the name of the config file");
+        printf("Usage: ./client <config.json>\n");
+    }
+
+    char *filename = argv[1];
+
+    struct config_struct *config = parse_JSON(argv[1]);
     in_addr_t server_ip = config->Server_IP;
     int tcp_port = config->TCP_Port;
     int udp_dest_port = config->Dest_UDP_Port;
     int payload_size = config->UDP_Payload_Size;
     int num_packets = config->Number_UDP_Packets;
     int udp_ttl = config->UDP_TTL;
+    int inter_mes = config->Inter_Measurement_Time;
+
 
 
     // printf("STRUCT SERVER IP: %d\n", serv);
@@ -149,7 +173,7 @@ int main() {
     int sockfd, connfd, udp_sockfd;
     struct sockaddr_in servaddr, udp_servaddr;
     FILE *fp;
-    char *filename = "../config.json";
+    // char *filename = "../config.json";
 
     //TCP socket create and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -157,9 +181,9 @@ int main() {
         perror("TCP Client socket creation failed...");
         exit(1);
     }
-    else {
-        printf("TCP Client socket created...\n");
-    }
+    // else {
+    //     printf("TCP Client socket created...\n");
+    // }
 
     //UDP socket
     udp_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -167,66 +191,39 @@ int main() {
         perror("UDP Client socket error");
         exit(1);
     }
-    else {
-        printf("UDP Client socket created\n");
-    }
+    // else {
+    //     printf("UDP Client socket created\n");
+    // }
 
     int df_val = IP_PMTUDISC_DO;
     if (setsockopt(udp_sockfd, IPPROTO_IP, IP_MTU_DISCOVER, &df_val, sizeof(df_val)) < 0){
         perror("UDP setsockopt failed");
         exit(1);
     }
-    else {
-        printf("UDP setsockopt successful!\n");
-    }
+    // else {
+    //     printf("UDP setsockopt successful!\n");
+    // }
 
     
     // bzero(&servaddr, sizeof(servaddr));
-    printf("Assigning IP and ports.\n");
+    // printf("Assigning IP and ports.\n");
 
     // assign TCP IP, PORT
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = server_ip;
     servaddr.sin_port = htons(tcp_port);
-    // char port_buff[MAX];
-    // inet_ntop(AF_INET, &servaddr, port_buff, MAX);
-    printf("TCP IP: %d TCP port: %d\n", server_ip, tcp_port);
-    printf("Assigned IP and TCP port!\n");
-
-    // assign UDP IP, PORT
-    // memset(&udp_servaddr, 0, sizeof(udp_servaddr));
-    // udp_servaddr.sin_family = AF_INET;
-    // udp_servaddr.sin_addr.s_addr = server_ip;
-    // printf("Client UDP IP: %d\n", server_ip);
-    
-    
-
-    // get high-entropy data ready
-    struct udp_payload{
-        short packet_id;
-        char payload[payload_size - 2];
-    };
-    struct udp_payload packet;
-
-    FILE *random_fp = fopen("../random_file.txt", "r");
-    char rand_buffer[payload_size];
-    printf("Trying to fgets random file\n");
-    fgets(rand_buffer, sizeof(rand_buffer), random_fp);
-    printf("Rand array filled\n");
-    // close(random_fp);
-
     
     // TCP connect the client socket to server socket
-    printf("Tryng to connect...\n");
+    // printf("Tryng to connect...\n");
     connfd = connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
     if (connfd == -1) {
         printf("Client connection to server socket failed...\n");
         exit(1);
     }
-    else {
-        printf("Client connected successfully to Server socket\n");
-    }
+    // else {
+    //     printf("Client connected successfully to Server socket\n");
+    // }
     fp = fopen(filename, "r");
     if (fp == NULL) {
         perror("Client error in reading file");
@@ -235,53 +232,45 @@ int main() {
 
     // TCP PRE-PROBING
     pre_probing(sockfd, fp);
-    printf("Client sent file successfully!\n");
-    printf("Client closing connection..\n");
+    // printf("Client sent file successfully!\n");
+    // printf("Client closing connection..\n");
+    fclose(fp);
 
     close(sockfd);
 
     sleep(5);
 
     servaddr.sin_port = htons(udp_dest_port);
-    printf("UDP port: %d\n", servaddr.sin_port);
+    // printf("UDP port: %d\n", servaddr.sin_port);
+
+    bool high_entropy = false;
 
     // UDP TRAINS choo choo
     // Train 1: Low Entropy
-    // socklen_t server_len = sizeof(servaddr);
-    for (int i = 0; i <num_packets; i++){
-        // set packet id
-        packet.packet_id = i;
-        // clear packet payload
-        memset(packet.payload, 0, payload_size -2);
-        if (sendto(udp_sockfd, &packet, sizeof(packet), 0, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1) {
-            perror("UDP Sendto error");
-            printf("%s\n", strerror(errno));
-        }
-        
-    }
-    printf("Sent first train!\n");
 
-    sleep(10);
-
-    for (int i = 0; i <num_packets; i++){
-        // set packet id
-        packet.packet_id = i;
-        // clear packet payload
-        memset(packet.payload, 0, payload_size - 2);
-
-        //high entropy
-        strncpy(packet.payload, rand_buffer, payload_size - 2);
-        if (sendto(udp_sockfd, &packet, sizeof(packet), 0, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1) {
-            perror("UDP Sendto error");
-            printf("%s\n", strerror(errno));
-        }
-
+    int u1 = send_udp_trains(udp_sockfd, servaddr, num_packets, payload_size, high_entropy);
+    if (u1 < 0){
+        perror("Send UDP Train error...");
+        exit(1);
     }
     
-    printf("Data sent to client\n");
+    // printf("Sent first train!\n");
+
+    sleep(inter_mes);
+
+    high_entropy = true;
+
+    // }
+    int u2 = send_udp_trains(udp_sockfd, servaddr, num_packets, payload_size, high_entropy);
+    if (u2 < 0){
+        perror("Send UDP Train error...");
+        exit(1);
+    }
+
+    // printf("Data sent to client\n");
     close(udp_sockfd);
 
-    sleep(10);
+    sleep(inter_mes * 0.75 + 1);
 
     // TCP Post-Probing
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -289,28 +278,25 @@ int main() {
         perror("TCP Client socket creation failed...");
         exit(1);
     }
-    else {
-        printf("TCP Client socket created...\n");
-    }
+    // else {
+    //     printf("TCP Client socket created...\n");
+    // }
 
-    printf("Assigning IP and ports.\n");
+    // printf("Assigning IP and ports.\n");
 
-    // assign TCP IP, PORT
-    // memset(&servaddr, 0, sizeof(servaddr));
-    // servaddr.sin_family = AF_INET;
-    // servaddr.sin_addr.s_addr = server_ip;
+    // assign TCP PORT
     servaddr.sin_port = htons(tcp_port);
-    printf("Assigned IP and TCP port!\n");
+    // printf("Assigned IP and TCP port!\n");
 
-    printf("Tryng to connect...\n");
+    // printf("Tryng to connect...\n");
     connfd = connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
     if (connfd == -1) {
         printf("Client connection to server socket failed...\n");
         exit(1);
     }
-    else {
-        printf("Client connected successfully to Server socket\n");
-    }
+    // else {
+    //     printf("Client connected successfully to Server socket\n");
+    // }
 
     // TCP POST-PROBING
     int n;
@@ -328,12 +314,41 @@ int main() {
     
 
 
-    printf("TCP post-probing done!\n");
-    printf("Client closing connection..\n");
+    // printf("TCP post-probing done!\n");
+    // printf("Client closing connection..\n");
 
     close(sockfd);
 
     return 0;
 
+}
+
+int send_udp_trains(int udp_sockfd, struct sockaddr_in udp_servaddr, int num_packets, int payload_size, bool high_entropy) {
+  struct udp_payload{
+      unsigned short packet_id;
+      char payload[payload_size - 2];
+  };
+  struct udp_payload udp_packet;
+  FILE *random_fp = fopen("../random_file.txt", "r");
+  char rand_buffer[payload_size];
+  // printf("Trying to fgets random file\n");
+  fgets(rand_buffer, sizeof(rand_buffer), random_fp);
+  // printf("Rand array filled\n");
+
+  for (int i = 0; i < num_packets; i++){
+      // set packet id
+      udp_packet.packet_id = (unsigned short)i;
+      // clear packet payload
+      memset(udp_packet.payload, 0, payload_size -2);
+      if (high_entropy){
+        strncpy(udp_packet.payload, rand_buffer, payload_size - 2);
+      }
+      if (sendto(udp_sockfd, &udp_packet, sizeof(udp_packet), 0, (struct sockaddr*)&udp_servaddr, sizeof(udp_servaddr)) == -1) {
+          perror("UDP Sendto error");
+          printf("%s\n", strerror(errno));
+      }
+  }
+  // printf("Sent train!\n");
+  return 1;
 }
 
--- VM_files/server_part1.c
+++ VM_files/server_part1.c
@@ -12,21 +12,18 @@
 #define MAX 1500
 
 void pre_probing(int sockfd) {
-    int n;
+    int num_bytes;
     FILE *fp;
     char *filename = "server_config.json";
     char buffer[MAX];
 
+    // printf("Starting pre-probing\n");
+
     fp = fopen(filename, "w");
-    while(1) {
-        n = recv(sockfd, buffer, MAX, 0);
-        if (n <= 0){
-            break;
-            return;
-        }
-        fprintf(fp, "%s", buffer);
-        printf("%s\n", buffer);
-        bzero(buffer, MAX);
+    while ((num_bytes = recv(sockfd, buffer, MAX, 0)) > 0){
+        // printf("Num bytes: %d\n", num_bytes);
+        // printf("buffer: %s\n", buffer);
+        fprintf(fp, "%s\n", buffer);
     }
 
     fclose(fp);
@@ -70,7 +67,7 @@ struct config_struct* parse_JSON() {
     cJSON *Server_IP = cJSON_GetObjectItem(root, "Server_IP");
     if (cJSON_IsString(Server_IP)) {
         config->Server_IP = inet_addr(Server_IP->valuestring);
-        printf("Server IP struct: %d\n", config->Server_IP);
+        // printf("Server IP struct: %d\n", config->Server_IP);
     }
     else {
         printf("Shouldn't get here\n");
@@ -129,14 +126,6 @@ void post_probing (int sockfd, bool comp) {
     char *post_string;
     int n;
     char buffer[MAX];
-    // n = recv(sockfd, buffer, MAX, 0);
-    // if (n <= 0){
-    //         perror("Server Post-Probing recv failed...");
-    // }
-    // else {
-    //     printf("Post-Probing from Client: %s\n", buffer);
-    //     bzero(buffer, MAX);
-    // }
         
     if (comp) {
         post_string = "Compression Detected..\n";
@@ -155,10 +144,14 @@ void post_probing (int sockfd, bool comp) {
 }
 
 
-int main() {
-    char *ip = "192.168.64.2";
-    // todo change to argv[1]
-    int tcp_port = 2000;
+int main(int argc, char** argv) {
+     if (argc < 2){
+        perror("Please include the name of the config file");
+        printf("Usage: ./server <TCP Port>\n");
+    }
+
+    char *filename = argv[1];
+    int tcp_port = atoi(argv[1]);
 
     int sockfd, bindfd, new_sock, udp_sockfd;
     struct sockaddr_in server_addr, new_addr, udp_server_addr, udp_client_addr;
@@ -171,9 +164,9 @@ int main() {
         perror("Server error in creating socket...");
         exit(1);
     }
-    else {
-        printf("Server socket creation successful\n");
-    }
+    // else {
+    //     printf("Server socket creation successful\n");
+    // }
 
     //TCP setsock opt reuse addr and reuse port
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0){
@@ -198,28 +191,32 @@ int main() {
     if (bindfd == -1){
         perror("Server bind failed");
     }
-    printf("Server socket bind successful!\n");
+    // else {
+    //     printf("Server socket bind successful!\n");
+    // }
+    
 
-    if (listen(sockfd, 10) == 0) {
-        printf("Server listening...\n");
-    }
-    else {
+    if (listen(sockfd, 10) < 0) {
         perror("Server error listening...");
         exit(1);
+       
     }
+    // else {
+    //      printf("Server listening...\n");
+    // }
 
     addr_size = sizeof(new_addr);
     new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
-    printf("Server accepted!\n");
+    // printf("Server accepted!\n");
     
     //accept and write config file to server_config.
     pre_probing(new_sock);
     
     // Parse JSON for other info
     struct config_struct *config = parse_JSON();
-    printf("Server Payload Length: %d\n", config->UDP_Payload_Size);
-    printf("Server sent file successfully!\n");
-    printf("Server closing connection..\n");
+    // printf("Server sent file successfully!\n");
+    // printf("Server closing connection..\n");
+    sleep(2);
     close(sockfd);
 
     int udp_dest_port = config->Dest_UDP_Port;
@@ -227,6 +224,7 @@ int main() {
     int num_packets = config->Number_UDP_Packets;
     int udp_ttl = config->UDP_TTL;
     in_addr_t server_ip = config->Server_IP;
+    int inter_mes = config->Inter_Measurement_Time;
 
 
     // Setup UDP
@@ -235,14 +233,14 @@ int main() {
         perror("UDP Server socket error");
         exit(1);
     }
-    else {
-        printf("UDP Server socket created\n");
-    }
+    // else {
+    //     printf("UDP Server socket created\n");
+    // }
     memset(&udp_server_addr, '\0', sizeof(udp_server_addr));
     udp_server_addr.sin_family = AF_INET;
     udp_server_addr.sin_port = htons(udp_dest_port);
     udp_server_addr.sin_addr.s_addr = server_ip;
-    printf("Server UDP IP: %d\n", server_ip);
+    // printf("Server UDP IP: %d\n", server_ip);
 
     int n;
     n = bind(udp_sockfd, (struct sockaddr*)&udp_server_addr, sizeof(udp_server_addr));
@@ -250,14 +248,14 @@ int main() {
         perror("UDP Server bind error");
         exit(1);
     }
-    else {
-        printf("UDP Server bind successful\n");
-    }
-    printf("UDP Server awaiting data...\n");
+    // else {
+    //     printf("UDP Server bind successful\n");
+    // }
+    // printf("UDP Server awaiting data...\n");
 
     // set timeout, tv_sec is timeout in seconds
     struct timeval timeout;
-    timeout.tv_sec = 8;
+    timeout.tv_sec = inter_mes * 0.75;
     timeout.tv_usec = 0;
     if (setsockopt(udp_sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) == -1) {
         perror("UDP setsockopt timeout");
@@ -269,20 +267,20 @@ int main() {
 
     int train = 0;
     bool first_received = false;
-    char payload_buffer[1500];
+    char payload_buffer[payload_size];
 
     struct udp_payload{
-        short packet_id;
-        char payload[1500-2];
+        unsigned short packet_id;
+        char payload[payload_size-2];
     };
 
     int train0_packets = 0;
     int train1_packets = 0;
 
     while(1){
-        bzero(payload_buffer, 1500);
+        bzero(payload_buffer, payload_size);
         addr_size = sizeof(udp_client_addr);
-        int rv = recvfrom(udp_sockfd, payload_buffer, 1500, 0, (struct sockaddr*)&udp_client_addr, &addr_size);
+        int rv = recvfrom(udp_sockfd, payload_buffer, payload_size, 0, (struct sockaddr*)&udp_client_addr, &addr_size);
         if (train == 0){
             if (!first_received){
                 // low_start = clock();
@@ -314,11 +312,11 @@ int main() {
             }
             else {
                 if (errno == EAGAIN){
-                    printf("UDP Timeout\n");
+                    // printf("UDP Timeout\n");
                     first_received = false;
                     if (train == 0){
                         train = 1;
-                        printf("Train set to: %d\n\n", train);
+                        // printf("Train set to: %d\n\n", train);
                         continue;
                     }
                     else {
@@ -338,21 +336,21 @@ int main() {
         // printf("Data received from Client\n");
     }
 
-    printf("Train 0: %d\n", train0_packets);
-    printf("Train 1: %d\n", train1_packets);
+    // printf("Train 0: %d\n", train0_packets);
+    // printf("Train 1: %d\n", train1_packets);
 
-    printf("Low start: %ld\n", low_start.tv_sec);
-    printf("Low ends: %ld\n", low_end.tv_sec);
-    double low_diff = ((low_end.tv_usec - low_start.tv_usec) / 1000);
-    printf("Low Diff: %f\n", low_diff);
-    printf("High start: %ld\n", high_start.tv_sec);
-    printf("High end: %ld\n", high_end.tv_sec);
-    double high_diff =  ((high_end.tv_usec - high_start.tv_usec) / 1000);
-    printf("High Diff: %f\n", high_diff);
+    // printf("Low start: %ld\n", low_start.tv_sec);
+    // printf("Low ends: %ld\n", low_end.tv_sec);
+    long double low_diff = (low_end.tv_sec*1000 + low_end.tv_usec/1000) - (low_start.tv_sec*1000 + low_start.tv_usec/1000);
+    // printf("Low Diff: %Lf\n", low_diff);
+    // printf("High start: %ld\n", high_start.tv_sec);
+    // printf("High end: %ld\n", high_end.tv_sec);
+    long double high_diff =  (high_end.tv_sec*1000  + high_end.tv_usec/1000) - (high_start.tv_sec*1000 - high_start.tv_usec/1000);
+    // printf("High Diff: %Lf\n", high_diff);
 
-    double difference = abs(high_diff - low_diff);
+    long double difference = abs(high_diff - low_diff);
     
-    printf("Difference between high and low: %f\n", difference);
+    printf("Difference between high and low: %Lf\n", difference);
     bool compression = false;
     if (difference < 100){
         printf("No Compression!\n");
@@ -362,7 +360,7 @@ int main() {
         printf("Compression Detected!\n");
     }
 
-    printf("Exited while loop!\n");
+    // printf("Exited while loop!\n");
 
     
     // TCP Post-Probing
@@ -371,9 +369,9 @@ int main() {
         perror("Server error in creating socket...");
         exit(1);
     }
-    else {
-        printf("Server socket creation successful\n");
-    }
+    // else {
+    //     printf("Server socket creation successful\n");
+    // }
 
     //TCP setsock opt reuse addr and reuse port
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0){
@@ -384,28 +382,26 @@ int main() {
     }
 
     // TCP serv_addr
-    // memset(&server_addr, 0, sizeof(server_addr));
-    // server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(tcp_port);
-    // server_addr.sin_addr.s_addr = INADDR_ANY;
 
     bindfd = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
     if (bindfd == -1){
         perror("Server bind failed");
     }
-    printf("Server socket bind successful!\n");
+    // printf("Server socket bind successful!\n");
 
-    if (listen(sockfd, 10) == 0) {
-        printf("Server listening...\n");
-    }
-    else {
+    if (listen(sockfd, 10) < 0) {
         perror("Server error listening...");
         exit(1);
+       
     }
+    // else {
+    //      printf("Server listening...\n");
+    // }
 
     addr_size = sizeof(new_addr);
     new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
-    printf("Server accepted!\n");
+    // printf("Server accepted!\n");
 
     post_probing(new_sock, compression);
     close(sockfd);
